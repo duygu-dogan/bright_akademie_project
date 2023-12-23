052bb96d3b6c5ae9950e55f03e8b62a82bb63b0a
duygu-dogan <duygudogan1509@gmail.com>
2023-12-24 00:12:13 +0300

    course card designs added

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Container, Card, Col, Row, CardImg, Button } from 'react-bootstrap'
+import { Container, Card, Col, Row, CardImg, Button, Image } from 'react-bootstrap'
 import BgTechnoMain from '../../Images/BgTechno0.jpg'
 import logoImg from '../../Images/whatsapp.png'
 import { FloatingWhatsApp } from 'react-floating-whatsapp'
 import Footer from '../../Components/Footer/Footer'
 import './Courses.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLaptopCode } from '@fortawesome/free-solid-svg-icons'
+import { faCloudArrowUp, faDatabase, faLaptopCode, faShieldHalved } from '@fortawesome/free-solid-svg-icons'
 
 const Courses = () => {
   return (
@@ -15,10 +15,10 @@ const Courses = () => {
         
         <div className='bg-img2' style={{ backgroundImage: `url(${BgTechnoMain})`, backgroundRepeat: 'no-repeat', backgroundSize: "cover" }}>
           <FloatingWhatsApp phoneNumber='+905558109862' accountName='Bright Akademie' statusMessage='Online' style={{ width: '150px', height: '150px' }} allowClickAway='true' avatar={logoImg} />
-          <Row xs={1} md={3} className="g-5 mx-5">
+          <Row xs={1} lg={3} className="g-5 mx-5 mb-4">
               <Col>
                 <Card>
-                
+                <div className='course-logos'><FontAwesomeIcon icon={faLaptopCode}/></div>
                   <Card.Body>
                   <Card.Title>Full Stack Programming Training</Card.Title>
                     <Card.Text>
@@ -26,13 +26,13 @@ const Courses = () => {
                       lead-in to additional content. This content is a little bit
                       longer.
                     </Card.Text>
-                  <Button variant="outline-info">More Details</Button>
+                  <Button>More Details</Button>
                   </Card.Body>
                 </Card>
               </Col>
             <Col>
               <Card>
-                
+                <div className='course-logos'><FontAwesomeIcon icon={faCloudArrowUp} /></div>
                 <Card.Body>
                   <Card.Title>Network, System and Cyber Security Expertise Training</Card.Title>
                   <Card.Text>
@@ -40,13 +40,13 @@ const Courses = () => {
                     lead-in to additional content. This content is a little bit
                     longer.
                   </Card.Text>
-                  <Button variant="outline-secondary">Secondary</Button>
+                  <Button>More Details</Button>
                 </Card.Body>
               </Card>
             </Col>
             <Col>
               <Card>
-               
+                <div className='course-logos'><FontAwesomeIcon icon={faShieldHalved}/></div>
                 <Card.Body>
                   <Card.Title>Cloud Computing Systems Training</Card.Title>
                   <Card.Text>
@@ -54,7 +54,7 @@ const Courses = () => {
                     lead-in to additional content. This content is a little bit
                     longer.
                   </Card.Text>
-                  <Button variant='outline-info' >More Details</Button>
+                  <Button>More Details</Button>
                 </Card.Body>
               </Card>
             </Col>
