238092e47a2965eb94ae68753be58d662687f562
duygu-dogan <duygudogan1509@gmail.com>
2023-12-24 23:27:11 +0300

    Started to Instructors Page

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import { Container, Image } from 'react-bootstrap'
-import logo from '../../Images/HeaderLogo.png'
+// import logo from '../../Images/HeaderLogo.png'
 import { faFacebook, faInstagram, faXTwitter } from '@fortawesome/free-brands-svg-icons'
 import './Footer.css'
 const Footer = () => {
@@ -11,9 +11,9 @@ const Footer = () => {
               <div className="col-md-4 d-flex align-items-center">
                   <span className="mb-3 mb-md-0 text-muted">© 2023 DDesign, Inc</span>
               </div>
-              <div><a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+              {/* <div><a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
                   <Image className="logo" src={logo} />
-              </a></div>
+              </a></div> */}
 
               <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
                   <li className="ms-3"><a className="text-muted" href="/"><FontAwesomeIcon className="icons" style={{ width: "24", height: "24"}} icon={faXTwitter}/></a></li>
--- src/Pages/Instructors/Instructors.js
+++ src/Pages/Instructors/Instructors.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
-import logo from '../../Images/HeaderLogo.png'
-import BgTechno from '../../Images/BgTechno.jpg'
+import logoImg from '../../Images/whatsapp.png'
+import BgTechnoMain from '../../Images/BgTechno0.jpg'
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
+import Footer from '../../Components/Footer/Footer'
+import InstructorCard from '../../Components/InstructorCard/InstructorCard'
 
 const Instructors = () => {
   return (
     <>
       <Container fluid className='p-0 main-page'>
-        <div className='bg-img' style={{ backgroundImage: `url(${BgTechno})`, backgroundRepeat: 'no-repeat', backgroundSize: "cover" }}>
+        <div className='bg-img2' style={{ backgroundImage: `url(${BgTechnoMain})`, backgroundRepeat: 'no-repeat', backgroundSize: "cover" }}>
+          <FloatingWhatsApp phoneNumber='+905558109862' accountName='Bright Akademie' statusMessage='Online' style={{ width: '150px', height: '150px' }} allowClickAway='true' avatar={logoImg} />
+            <div>
+              <InstructorCard></InstructorCard>
+            </div>
+          <Footer></Footer>
         </div>
       </Container>
     </>
